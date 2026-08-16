b5ff1a62289eaa7d7df2eb5f8a6c3842affcbd68
kinqsy <kingbaby888@mail.ru>
2026-08-16 12:37:42 +0500

    notes.js

--- notes.js
+++ notes.js
@@ -1,6 +1,1 @@
 alert("JS WORKS");
-const url = "https://rgkfegdtxaojceknnzlr.supabase.co";
-const key = "sb_publishable_uK7zrVyq8AlHpoj13pGQ6g_q3L47Akw";
-const client = window.supabase.createClient(url, key);
-console.log(client);
-alert("SUPABASE CLIENT WORKS");
